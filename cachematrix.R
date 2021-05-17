8ee0aeda699bfa533408d217fb84ea08dc31f11e
pnagpal14 <pnagpal14@gmail.com>
2021-05-18 00:08:42 +0530

    Revert "Update cachematrix.R"
    This reverts commit 55cf9eb56e9d71bc651be2f4e4de1eb2105b3fcd.

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,28 @@
-library(datasets)
-data(iris)
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
 
 
+## Write a short comment describing this function
 
-str(str)
-str(rnorm)
-str(iris)
-set.seed(1)
-rnorm(5)
-rnorm(5)
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
+}
